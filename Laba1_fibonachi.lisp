1768db2eb3d2eafcfb29876a3149ebcf2c1dd91f
Nick <nikitamm59rus@gmail.com>
2020-02-29 13:23:29 +0300

    Laba 1. Task 2

--- Laba1_fibonachi.lisp
+++ Laba1_fibonachi.lisp
@@ -1,17 +1,13 @@
 (defun fib(i)
-  (if (= i 1) 
-  1
-  (if (= i 2)
-  1
-  (+ (fib (- i 1)) (fib (- i 2))))))
+   (if (= i 1)                   
+     0
+     (if (= i 2)
+       1
+       (+ (fib (- i 1)) (fib (- i 2))))))
 
-(defun solve(i)
-  (let ((f (fib i)))
-  (print f)
-  (if (< 4000000 f)
-  0 ;return
-  (if (= 0 (mod f 2))
-  (+ f (solve (+ i 1)))
-  (solve (+ i 1))))))
-
-(print (solve 1))
+(loop for i from 1 to 16     
+      do (format t " ~D~%" (fib i))) 
+ 
+(loop for i from 1 to 16     
+      do (format t "~D," (fib i))     
+      finally (format t "..~%"))
