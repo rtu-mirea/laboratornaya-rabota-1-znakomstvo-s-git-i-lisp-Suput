5bcffabe3e77e2185c268c265ca04a670d4c85ec
Nick <40500378+Suput@users.noreply.github.com>
2020-05-23 16:31:28 +0400

    Wrapped into function

--- lab2/task2.lisp
+++ lab2/task2.lisp
@@ -1,10 +1,11 @@
-(let ((in (open "file.txt" :if-does-not-exist nil)))
-   (when in
-      (loop for line = (read-line in nil)
-      
-      while line do (format t "~a~%" line))
-      (close in)
+(defun openAndReadFile (path)
+   (let ((in (open path :if-does-not-exist nil)))
+      (when in
+         (loop for line = (read-line in nil)
+
+         while line do (format t "~a~%" line))
+         (close in)
+      )
    )
 )
-
 ; wasn't tested as ".\ros.exe run" stopped to work
